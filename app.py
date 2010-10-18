8af0fe7f99269d488e4a30eb62f4c2731fe14c40
Linmiao Xu <linmiao.xu@gmail.com>
2010-10-17 19:37:31 -0700

    Add routes for showing different amounts of data

--- app.py
+++ app.py
@@ -3,10 +3,30 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.debug = True
 
+
+all_companies = list(enumerate(Company.query.all()))
+# filtered = Company.query.filter(Company.url > '').filter(Company.dead == False).filter(Company.exited == False)
+# dead = Company.query.filter(Company.url > '').filter(Company.dead == False)
+filtered = []
+exited = []
+for i,company in all_companies:
+    if company.url > '' and company.dead == False and company.exited == False:
+        filtered.append((i,company))
+    if company.exited == True:
+        exited.append((i,company))
+
+
 @app.route('/')
-def index():
-    companies = Company.query.all()
-    return render_template('index.html', companies=companies, enumerate=enumerate)
+def show_active():
+    return render_template('index.html', companies=filtered)
+
+@app.route('/all')
+def show_all():
+    return render_template('index.html', companies=all_companies)
+
+@app.route('/exited')
+def show_exited():
+    return render_template('index.html', companies=exited)
 
 
 if __name__ == '__main__':
