f1d92ab45110fbeea7905581af7532924fb451d0
Linmiao Xu <linmiao.xu@gmail.com>
2010-12-30 21:12:34 -0800

    Clean up existing code. Start tracking favicons.

--- models.py
+++ models.py
@@ -1,3 +1,9 @@
+from datetime import datetime, timedelta
+import urlparse
+import locale
+import os
+import re
+
 from elixir import *
 import mechanize
 import urllib2
@@ -7,35 +13,72 @@ from radiant.browser import Browser
 from radiant.interfaces.alexa import get_alexa_rank
 from radiant.interfaces.pagerank import get_pagerank
 
-import urlparse
-import locale
-import os
-import re
-
-
 locale.setlocale(locale.LC_ALL, '')
+
+favicon_dir = 'data/favicons'
+valid_filetypes = [
+    'image/vnd.microsoft.icon',
+    'image/x-icon',
+    'image/png',
+    'image/gif',
+    'application/octet-stream'
+]
+
 
 class Company(Entity):
     using_options(tablename='companies')
     using_table_options(useexisting=True)
 
-    has_field('name',           String(32))
-    has_field('class_year',     Date)
-    has_field('hostname',       String(128))
-    has_field('url',            String(128))
-    has_field('favicon_url',    String(128))
-    has_field('title',          String(128))
-    has_field('meta_desc',      String(128))
-    has_field('pagerank',       Integer)
-    has_field('alexa',          Integer)
-    has_field('dead',           Boolean)
-    has_field('exited',         Boolean)
-    has_field('aq_price',       Integer)
+    has_field('id',                 Integer, primary_key=True)
+    # has_field('name',               String(32), unique=True)
+    has_field('name',               String(32))
+    has_field('class_year',         Date)
+    has_field('hostname',           String(128))
+    has_field('url',                String(128))
+    has_field('dead',               Boolean)
+    has_field('exited',             Boolean)
+    has_field('aq_price',           Integer)
+    has_field('notes',              Text)
+    has_field('data_checked_at',    DateTime)
+    has_field('favicon_url',        String(128))
+    has_field('favicon_filename',   String(128))
+    has_field('title',              String(128))
+    has_field('meta_desc',          String(128))
+    has_field('pagerank',           Integer)
+    has_field('alexa',              Integer)
+    has_field('stats_checked_at',   DateTime)
+
+    def _update_alexa(self):
+        try:
+            if self.url:
+                alexa = get_alexa_rank(self.url.replace('https://', 'http://'))
+                print '%s - Alexa: %s' % (self.url, alexa)
+                self.alexa = alexa
+            else:
+                print '%s has invalid URL!' % self.name
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            print '%s fucked up!!!' % self.url
+
+    def _update_pagerank(self):
+        try:
+            if self.url:
+                pr = get_pagerank(self.url)
+                print '%s PR: %s' % (self.url, pr)
+                self.pagerank = pr
+            else:
+                print '%s has invalid URL!' % self.name
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            print '%s fucked up!!!' % self.url
 
     @staticmethod
     def scrape_all():
         b = Browser()
         for company in Company.query.filter(Company.url > '').filter(Company.dead == False):
+            print 'Checking: %s' % company.url
             try:
                 url = 'http://www.%s' % company.hostname
                 print 'Checking: %s' % url
@@ -69,7 +112,8 @@ class Company(Entity):
             try:
                 response = urllib2.urlopen(favicon_url)
                 # if any(i in response.headers.getheader('content-type') for i in ['image/x-icon', 'image/png']):
-                open('icons/%s.ico' % urlparse.urlsplit(company.url).netloc, 'w').write(response.read())
+                favicon_filename = '%s.ico' % urlparse.urlsplit(company.url).netloc
+                open(os.path.join(favicon_dir, favicon_filename), 'w').write(response.read())
             except urllib2.HTTPError:
                 print '404 - %s' % favicon_url
             except urllib2.URLError:
@@ -77,6 +121,7 @@ class Company(Entity):
             except TypeError:
                 print 'TypeError WTF - %s' % favicon_url
             else:
+                company.favicon_filename = favicon_filename
                 company.favicon_url = favicon_url
             session.commit()
 
@@ -84,58 +129,33 @@ class Company(Entity):
     def update_all_stats():
         companies = Company.query.filter(Company.url > '').filter(Company.dead == False)
         for company in companies:
-            company.update_pagerank()
-            company.update_alexa()
-            session.commit()
+            if not company.stats_checked_at or company.stats_checked_at < datetime.now()-timedelta(days=1):
+                company._update_pagerank()
+                company._update_alexa()
+                company.stats_checked_at = datetime.now()
+                session.commit()
 
     @staticmethod
     def convert_and_merge_favicons():
         icon_names = [f for f in os.listdir('icons') if f.endswith('.ico')]
-        for i in icon_names:
-            old_icon = 'icons/%s' % i
-            new_icon = 'icons/%s.png' % i[:-4]
+        for name in icon_names:
+            old_icon = os.path.join(favicon_dir, name)
+            new_icon = os.path.join(favicon_dir, '%s.png' % name[:-4])
             os.system('convert %s[0] -resize 16x16 -flatten %s' % (old_icon, new_icon))
         companies = Company.query.all()
         favicon_sequence = []
         num_valid = 0
         for company in companies:
             host = urlparse.urlsplit(company.url).netloc
-            path = 'icons/%s.png' % host
+            path = os.path.join(favicon_dir, '%s.png' % host)
             if os.path.isfile(path):
                 favicon_sequence.append(path)
                 num_valid += 1
             else:
-                print 'Not valid: icons/%s.png' % host
-                print company.url
+                print '%s favicon invalid: %s' % (company.url, path)
                 favicon_sequence.append('public/img/blank.png')
         print '#valid: %d' % num_valid
         os.system('convert %s +append public/img/favicons.png' % ' '.join(favicon_sequence))
-
-    def update_alexa(self):
-        try:
-            if self.url:
-                alexa = get_alexa_rank(self.url.replace('https://', 'http://'))
-                print '%s - Alexa: %s' % (self.url, alexa)
-                self.alexa = alexa
-            else:
-                print '%s has invalid URL!' % self.name
-        except (KeyboardInterrupt, SystemExit):
-            raise
-        except:
-            print '%s fucked up!!!' % self.url
-
-    def update_pagerank(self):
-        try:
-            if self.url:
-                pr = get_pagerank(self.url)
-                print '%s PR: %s' % (self.url, pr)
-                self.pagerank = pr
-            else:
-                print '%s has invalid URL!' % self.name
-        except (KeyboardInterrupt, SystemExit):
-            raise
-        except:
-            print '%s fucked up!!!' % self.url
 
     def formatted_date(self):
         return self.class_year.strftime('%Y/%m') if self.class_year else ''
--- tmp/clean_shit_icons.sh
+++ tmp/clean_shit_icons.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-find "public" -name "*.ico" -exec file {} \; | egrep "(text|empty)" | sed 's/://' | cut -d' ' -f1 | xargs -n1 rm
+find "data/favicons" -name "*.ico" -exec file {} \; | egrep "(text|empty)" | sed 's/://' | cut -d' ' -f1 | xargs -n1 rm
