b52b4aab6a855be3565e9e280bf5c31af5a4df7e
Linmiao Xu <linmiao.xu@gmail.com>
2012-10-15 20:43:06 -0700

    Restart yclist as a rails 3.2 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yclist::Application.config.secret_token = '61d66cab3032cceec680e1e728f0ff0db2eb91a3632867fb6a030ed032d50fa4cb23f365b94790cfade146ebe5b7377b4c6a9f1ac0dd5641a82f1ae8e4540e00'
--- tmp/clean_shit_icons.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-find "data/favicons" -name "*.ico" -exec file {} \; | egrep "(text|empty)" | sed 's/://' | cut -d' ' -f1 | xargs -n1 rm
