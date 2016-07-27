f30b366371343b5fe6aa800dd3077ec54a355db4
Linmiao Xu <linmiao.xu@gmail.com>
2016-07-26 23:21:43 -0700

    Change some squatted domains

--- app/models/link_checker.rb
+++ app/models/link_checker.rb
@@ -6,12 +6,16 @@ class LinkChecker
     @companies = companies.select {|c| c.url.present? }
   end
 
+  def check_link(url)
+    final_url = `curl -sLI -o /dev/null -m 10 -w %{url_effective} "#{url}"`
+    if final_url.gsub(/\/\z/, '') != url
+      puts "#{url} -> #{final_url}"
+    end
+  end
+
   def check_all
     Parallel.each(@companies, :in_threads => 10) do |company|
-      final_url = `curl -sLI -o /dev/null -m 10 -w %{url_effective} "#{company.url}"`
-      if final_url.gsub(/\/\z/, '') != company.url
-        puts "#{company.url} -> #{final_url}"
-      end
+      check_link(company.url)
     end
     true
   end
