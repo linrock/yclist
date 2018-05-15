3a751b4912cbaca8bf4eb14f549790f5364feac9
Linmiao Xu <linmiao.xu@gmail.com>
2018-05-16 00:45:07 +0800

    Update gems, remove unused db files

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,11 @@
 require_relative 'boot'
 
-require 'rails/all'
-require 'active_model'
+require 'active_model/railtie'
+require 'action_controller/railtie'
+require 'action_view/railtie'
+require 'action_mailer/railtie'
+require 'rails/test_unit/railtie'
+require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
