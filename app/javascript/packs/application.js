55a7e9153e6323d34849cfb8fa88473650835c59
Linmiao Xu <linmiao.xu@gmail.com>
2018-05-30 08:22:05 +0800

    Use jquery and tablesorter via yarn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,53 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+const $ = require('jquery')
+const tablesorter = require('tablesorter')
+
+$.tablesorter.addParser({
+  id: 'name',
+  is: () => true,
+  format: (s) => s.toLowerCase(),
+  type: 'text',
+})
+
+$.tablesorter.addParser({
+  id: 'cohort',
+  is: () => true,
+  format: (s) => {
+    if (s[0] == "F") {
+      return "165"
+    } else {
+      return `${s.slice(1)}${s[0] === "W" ? 0 : 9}`
+    }
+  },
+  type: 'numeric',
+});
+
+$(() => {
+  $('table').tablesorter({
+    headers: {
+      1: { sorter: 'name', },
+      3: { sorter: 'cohort' },
+      5: { sorter: false }
+    }
+  })
+
+  updateCompanyCount = () => $("#companies_count").text($("tbody > tr:visible").length)
+
+  $.each(["operating", "exited", "dead"], (i,status) => {
+    function toggleStatus() {
+      const $e = $(`.${status}`)
+      if ($(this).is(":checked")) {
+        $e.show()
+      } else {
+        $e.hide()
+      }
+      updateCompanyCount()
+    }
+
+    $(`#toggle-${status}`).on("change", toggleStatus)
+  })
+
+  updateCompanyCount()
+})
