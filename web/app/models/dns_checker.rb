620be8d30d979256e9832158fcd7d90e5dfaa80e
Linmiao Xu <linmiao.xu@gmail.com>
2018-06-18 14:09:31 +0800

    Verbose dns checks by default

--- web/app/models/dns_checker.rb
+++ web/app/models/dns_checker.rb
@@ -8,22 +8,24 @@ class DnsChecker
   end
 
   # true if successful, false if dns failure
-  def self.check_dns(url)
+  def self.valid_dns?(url)
     host = url =~ /\Ahttps?:\/\// ? URI.parse(url).host : url
-    host_query = `host "#{host}" 2>&1`.strip
+    host_query = `host -W 5 "#{host}" 2>&1`.strip
     host_query.present?
   end
 
-  def check!
+  def check
     results = {
       successes: [],
       failures: [],
     }
     Parallel.each(companies_to_check, in_threads: CONCURRENCY) do |company|
       url = company.url
-      if self.class.check_dns(url)
+      puts "Checking #{url}"
+      if self.class.valid_dns?(url)
         results[:successes] << url
       else
+        puts "#{url} is invalid!"
         results[:failures] << url
       end
     end
